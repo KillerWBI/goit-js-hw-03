fad42a7d74a0168539ca437d148bfc8d6525b76e
GetUp <108660418+KillerWBI@users.noreply.github.com>
2025-05-06 13:53:47 +0200

    3

---
+++ js/task-2.js
@@ -0,0 +1,18 @@
+function makeArray(firstArray, secondArray ,maxLength) {
+const ArraySum = firstArray.concat(secondArray);
+
+if (ArraySum.length > maxLength ) {
+    return ArraySum.slice(0,maxLength) ;
+}
+
+return ArraySum
+}
+
+
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
